ccb9006236b3701b466e7fec44c55dcab7fe24a0
Stanislao <stanislaoweb@libero.it>
2020-11-09 14:35:53 +0100

    Inizio

---
+++ main.js
@@ -0,0 +1,20 @@
+// Stiamo prendendo sempre più confidenza con Vue e oggi abbiamo visto come fare il carousel di immagini con Vue!
+// Oggi pomeriggio rifatelo anche voi e, se vi fa piacere e ne avete il tempo, aggiungete come bonus anche l'autoplay con una timing functions.
+// Piccolo consiglio: cercate nella documentazione di vue come fare per eseguire una funzione quando l'istanza di Vue è stata creata.
+
+var app = new Vue({
+
+  el: '#root',
+
+  data: {
+
+  },
+
+  methods: {
+    
+
+  },
+
+
+
+});
