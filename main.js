0850ad382b859b462fa208f4a8fbf587ef34eae3
Stanislao <stanislaoweb@libero.it>
2020-11-09 15:28:09 +0100

    Aggiunte le foto, Vue ed array

--- main.js
+++ main.js
@@ -2,16 +2,26 @@
 // Oggi pomeriggio rifatelo anche voi e, se vi fa piacere e ne avete il tempo, aggiungete come bonus anche l'autoplay con una timing functions.
 // Piccolo consiglio: cercate nella documentazione di vue come fare per eseguire una funzione quando l'istanza di Vue è stata creata.
 
+
 var app = new Vue({
 
   el: '#root',
 
   data: {
 
+    indexImages: 0,
+    images: [
+      "https://i2.wp.com/movingtips.wpengine.com/wp-content/uploads/2019/07/new-york2.jpg?fit=1024%2C684&ssl=1",
+      "https://multiplayer.net-cdn.it/thumbs/images/2018/09/14/cities-skylines-switch_1_jpg_800x0_crop_upscale_q85.jpg",
+      "https://hbr.org/resources/images/article_assets/2016/01/04-Cities-Looking-to-Harness-Smart-Technologies-Should-Start-Small_960px-x-540px.jpg",
+      "https://assets.unenvironment.org/2020-02/sustainable-cities.jpg",
+      "https://graylinegroup.com/wp-content/uploads/2016/03/city-1209105_1280-1.jpg"
+    ]
+
   },
 
   methods: {
-    
+
 
   },
 
